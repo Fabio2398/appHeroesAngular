b8443ef61a04cb7bf7d06a00b960efb2dd585ccb
Fabio Stella <Fabio.Stella@eng.it>
2019-02-18 17:38:32 +0100

    second commit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './classes/hero';
-import { HEROES } from './heroes/mock-heroes';
+//import { HEROES } from './heroes/mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
 
   /*
   getHeroes(): Hero[] {
     return HEROES;
   }*/
 
+  /** Log a HeroService message with the MessageService */
+private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+}
+
+private heroesUrl = 'api/heroes';  // URL to web api
+
   getHeroes(): Observable<Hero[]> {
     //invia un messaggio dopo aver recuperato l'eroe
     this.messageService.add('HeroService: eroe preso');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
